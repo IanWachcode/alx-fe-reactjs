19017ff1c8a9f90aa83b33f48284912cb8072846
IanWachcode <ianwachira90@gmail.com>
2026-01-10 19:30:32 +0300

    third task commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
-const UserProfile = (user) => {
+const UserProfile = (props) => {
   return (
     <div>
-        <h2>{user.name}</h2>
-        <p>Age: {user.age}</p>
-        <p>Bio: {user.bio}</p>
+        <h2>{props.name}</h2>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
     </div>
     );
 };
