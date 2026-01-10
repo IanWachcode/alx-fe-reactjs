f8d602d8a5595c2ea406dedd596b8231cf54bc4f
IanWachcode <ianwachira90@gmail.com>
2026-01-10 19:26:26 +0300

    3rd task commit

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile = (user) => {
+  return (
+    <div>
+        <h2>{user.name}</h2>
+        <p>Age: {user.age}</p>
+        <p>Bio: {user.bio}</p>
+    </div>
+    );
+};
+
+export default UserProfile;
