219fcd2f545a6e2aeef270d1a4ec45d1a0bda854
IanWachcode <ianwachira90@gmail.com>
2026-01-16 08:17:34 +0300

    style commit task2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +16,10 @@ function App() {
     <>
       <WelcomeMessage />
       <Header />
-      <MainContent />
+   s   <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
+      <Counter />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
