7a2e5df929e6a760a01feb5b1d7480ae49631b18
IanWachcode <ianwachira90@gmail.com>
2026-02-08 15:12:54 +0300

    tailwind taks final comms

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile () {
     return (
-        <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center hover:shadow-xl transition-shadow duration-300">
+        <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center hover:shadow-xl transition-shadow duration-300">
             <img src="https://via.placeholder.com/150" alt="User" className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"/>
             <h1 className="text-xl text-blue-800 my-4 font-semibold hover:text-blue-500 transition-colors duration-300">John Doe</h1>
             <p className="text-sm text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
