6cbbfc7774fddb68e5b628c01adc086c7115fa4f
IanWachcode <ianwachira90@gmail.com>
2026-02-11 23:24:37 +0300

    task 1 comms

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,24 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import AddRecipe from "./pages/AddRecipe";
-import RecipeDetails from "./pages/RecipeDetails";
-import "./index.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddRecipe />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <HomePage />;
 }
 
 export default App;
