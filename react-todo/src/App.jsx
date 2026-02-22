572491344fb6dec3c18bc5205d766c2fd7bdfc52
IanWachcode <ianwachira90@gmail.com>
2026-02-22 22:30:59 +0300

    error handling

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,9 +1,11 @@
 import TodoList from "./components/TodoList";
+import TodoListTest from "./components/tests/TodoList.test";
 
 function App() {
   return (
     <div>
       <TodoList />
+      <TodoListTest />
     </div>
   );
 }
